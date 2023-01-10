217e698dfb792a2d6f47f7274826541440b248dd
Benjamin <benjamin.joham@gmail.com>
2023-01-10 16:46:02 +0100

    simplify app component

--- src/App.vue
+++ src/App.vue
@@ -8,68 +8,22 @@
   <main>
     <HeroWrapper />
     <div class="my-5">
-      <ProductDetails :details="activeProduct?.productDetails" />
-      <ProductSpecifications :details="activeProduct?.productSpecifications" />
+      <ProductDetails :details="data?.productDetails" />
+      <ProductSpecifications :details="data?.productSpecifications" />
     </div>
     <RelatedProducts />
   </main>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import NavBar from './components/Header/NavBar.vue'
 import SystemBar from './components/Header/SystemBar.vue'
 import HeroWrapper from './components/HeroWrapper.vue'
 import ProductDetails from './components/ProductDetails.vue'
 import ProductSpecifications from './components/ProductSpecifications.vue'
 import RelatedProducts from './components/RelatedProducts.vue'
-import { type State, state } from './store'
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
-export default {
-  name: 'App',
-  components: {
-    HeroWrapper,
-    NavBar,
-    SystemBar,
-    RelatedProducts,
-    ProductDetails,
-    ProductSpecifications,
-  },
-  setup(): Pick<State, 'activeProduct'> {
-    const data = state.products.find(({ isActive }) => isActive) || null
-    state.setActiveProduct(data)
-    return {
-      activeProduct: state.activeProduct,
-    }
-  },
-}
+import { state } from './store'
+
+const data = state.products.find(({ isActive }) => isActive) || null
+state.setActiveProduct(data)
 </script>
-
-<style scoped>
-header {
-  padding: 0 8%;
-}
-/* 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
-</style>
